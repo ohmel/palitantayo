1de16f3f5cf5340ddc7735e78261c3167608e441
Ohmel Paguirigan <whooopz@gmail.com>
2015-09-17 21:18:31 +0800

    comment

--- app/js/controllers/profileController.js
+++ app/js/controllers/profileController.js
@@ -1,18 +1,25 @@
 /**
  * Created by Ohmel on 7/29/2015.
  */
-ptApp.controller('profileController', function ($location, $scope, Globals, $route, ngNotify, profileService, itemService) {
+ptApp.controller('profileController', function ($location, $scope, Globals, $route, ngNotify, profileService, itemService, commentService) {
     $scope.tooltipMessage = "Description Goes Here sdaf s sdf sdf sf saf sf sdf sdafsdafsdf asdfsda fsd adf sdf";
     $scope.globals = Globals;
     $scope.route = $route.current.params;
     $scope.profileService = profileService;
     $scope.itemService = itemService;
     //$scope.profile = {};
-    //$scope.items = [];
+    $scope.comments = [];
 
-    $scope.editProfile = function(){
+    $scope.editProfile = function () {
         $location.path("/editProfile/" + $scope.route.userId);
     }
+
+    commentService.getComments(
+        function (success) {
+            $scope.comments = success.data;
+        }, function (error) {
+            ngNotify.set(error.message, 'error')
+        }, $scope.route.userId, 'user');
 
     if (Globals.isNothing($scope.profile) == true) {
         profileService.getProfile(
