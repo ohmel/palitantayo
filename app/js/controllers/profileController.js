9661487c7201dee5ae1da0e203c7cbaa99fed83d
Ohmel Paguirigan <whooopz@gmail.com>
2015-09-23 06:16:43 +0800

    tiny mce and follow and unfollow schemes

--- app/js/controllers/profileController.js
+++ app/js/controllers/profileController.js
@@ -18,8 +18,13 @@ ptApp.controller('profileController', function ($location, $scope, Globals, $rou
     $scope.postComment = function(commentMessage){
         commentService.postComment(
             function(success){
-                if(Globals.isNothing($scope.comments) == true){
-                    $scope.comments = success.data;
+                if(Globals.isNothing($scope.comments) === true){
+                    commentService.getComments(
+                        function (success) {
+                            $scope.comments = success.data;
+                        }, function (error) {
+                            ngNotify.set(error.message, 'error')
+                        }, $scope.route.itemId, 'item');
                 }else{
                     $scope.comments.push(success.data);
                 }
